aa26e5c8f604a601ce9f5cff441ef37fb3960b6a
Álvaro <36482489+alvaroolt@users.noreply.github.com>
2019-12-03 09:15:53 +0100

    Leves correcciones

--- js/canvas.js
+++ js/canvas.js
@@ -16,28 +16,25 @@ window.onload = function () {
     if (contexto) {
 
         x = 0;
-        y = 50;
+        y = 10;
         contexto.beginPath();
         contexto.moveTo(50 + x, 50 + y);
         contexto.lineTo(120 + x, 150 + y);
         contexto.lineTo(0 + x, 180 + y);
         contexto.lineTo(120 + x, 210 + y);
-        contexto.lineTo(50 + x, 310 + y);
-        contexto.lineTo(160 + x, 250 + y);
-        contexto.lineTo(190 + x, 370 + y);
-        contexto.lineTo(220 + x, 250 + y);
-        contexto.lineTo(330 + x, 310 + y);
+        contexto.lineTo(70 + x, 250 + y);
+        contexto.lineTo(310 + x, 250 + y);
         contexto.lineTo(260 + x, 210 + y);
         contexto.lineTo(380 + x, 180 + y);
         contexto.lineTo(260 + x, 150 + y);
         contexto.lineTo(330 + x, 50 + y);
         contexto.lineTo(220 + x, 110 + y);
-        contexto.lineTo(190 + x, -10 + y);
+        contexto.lineTo(190 + x, 125 + y);
         contexto.lineTo(160 + x, 110 + y);
         let lineGrad = contexto.createLinearGradient(-100 + x, 200 + y, 400 + x, 200 + y);
         lineGrad.addColorStop(0, '#fff');
-        lineGrad.addColorStop(0.05, 'blue');
-        lineGrad.addColorStop(0.6, 'green');
+        lineGrad.addColorStop(0.30, 'blue');
+        //lineGrad.addColorStop(0, 'green');
         lineGrad.addColorStop(1, 'red');
         contexto.fillStyle = lineGrad;
         contexto.fill();
